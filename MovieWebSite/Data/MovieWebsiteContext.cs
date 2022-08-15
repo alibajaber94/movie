9ec3691262593a272b6a44f8cf793cb5c12409e2
Ali bajaber <alibajaber94@gmail.com>
2022-08-16 01:12:01 +0300

    GUI git first push

---
+++ MovieWebSite/Data/MovieWebsiteContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieWebsite.Models;
+
+namespace MovieWebsite.Data
+{
+    public class MovieWebsiteContext : DbContext
+    {
+        public MovieWebsiteContext (DbContextOptions<MovieWebsiteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieWebsite.Models.Movie> Movie { get; set; } = default!;
+    }
+}
